61be3d401a077fa527e3e73daca9f01a5fd00d2a
cookiehacker <erwanaubry45@protonmail.com>
2021-01-04 18:07:21 +0100

    Create vbox simulation

--- src/main/java/vues/Simulation.java
+++ src/main/java/vues/Simulation.java
@@ -1,26 +1,17 @@
 package vues;
 
 import controlleurs.Controlleur;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import modele.InvalidJSONFileException;
-import org.json.simple.parser.ParseException;
-
 import java.io.IOException;
 import java.net.URL;
 
 public class Simulation {
     @FXML
-    public HBox monPane;
+    public VBox monPane;
 
     private Stage stage;
     private Controlleur controlleur;
