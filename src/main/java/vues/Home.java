f2c763fb4662ec38e76702f4503bdcbfcc4b08a1
nightlyside <nightlyside@gmail.com>
2021-01-06 10:22:25 +0100

    Starting adding nbiter

--- src/main/java/vues/Home.java
+++ src/main/java/vues/Home.java
@@ -244,4 +244,8 @@ public class Home {
     public void clearScreen(ActionEvent actionEvent) {
         linechart.getData().clear();
     }
+
+    public void getNbIterations(ActionEvent actionEvent) {
+        Console.getInstance().addLine("NbChanged");
+    }
 }
